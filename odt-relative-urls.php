84fae199911738551bcb998ad7655a53a2766c1e
Miguel Ayala <info@optimiza.mx>
2014-01-11 12:04:51 -0800

    License: Update license with author information.

--- odt-relative-urls.php
+++ odt-relative-urls.php
@@ -10,7 +10,7 @@
  * License: MIT License
  The MIT License (MIT)
 
-Copyright (c) [year] [fullname]
+Copyright (c) 2014 Miguel Ayala
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
